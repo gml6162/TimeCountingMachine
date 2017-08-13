d90564a0f5e60f71f534abb6e87c03846e3de77a
gml61 <gml61@DESKTOP-JNS3VFA>
2017-08-13 21:33:42 +0900

    ctrl

--- TimeCountingMachine/src/timeCountingMachine/ControlFrame.java
+++ TimeCountingMachine/src/timeCountingMachine/ControlFrame.java
@@ -36,9 +36,8 @@ public class ControlFrame extends JFrame {
 	private JPanel subButtonPanel = new JPanel(new GridLayout(3, 1, 0, 5));
 	private JPanel switchPersonPanel = new JPanel(new GridLayout(2, 1, 0, 5));
 	private JPanel mainPanel = new JPanel(new GridLayout(1, 3, 5, 5));
-
 	private JPanel userDataPanel = new JPanel();
-	
+
 	public static JToggleButton mainStartStopButton = new JToggleButton("START");
 
 	// private JToggleButton mainStopButton = new JToggleButton("STOP");
@@ -48,98 +47,73 @@ public class ControlFrame extends JFrame {
 	// private JToggleButton subStopButton = new JToggleButton("STOP");
 
 	private JButton upButton = new JButton("up");
-
 	private JButton downButton = new JButton("down");
-
 	private JLabel mainButtonLabel = new JLabel("total driving time");
-
 	private JLabel subButtonLabel = new JLabel("lap time");
 
 	public ControlFrame() {
-
 		super("Control");
-
 		this.setDefaultCloseOperation(ControlFrame.EXIT_ON_CLOSE);
-
 		this.setBounds(400, 200, 300, 300);
-
 		this.initialize();
-
 	}
-	
+
 	private void setUserDataPanel() {
-		String[] columnNames = {"Name", "Time"};
+		String[] columnNames = { "Name", "Time" };
 		String[][] data = new String[Main.userData.size()][2];
 		
-		data[0][0]="1";
-		data[0][1]="success";
-		
+		data[0][0] = "1";
+		data[0][1] = "success";
+
 		JTable userDataTable = new JTable(data, columnNames);
-		
-		
+
 		userDataPanel.add(userDataTable);
 	}
 
 	private void initialize() {
 
 		this.setVisible(false);
+		this.setUserDataPanel();
 
 		// this.add(mainPanel);
 
 		// Container mainPanelcontainer = new Container();
-
 		mainPanel.add(mainButtonPanel);
-
 		mainPanel.add(subButtonPanel);
-
-		mainPanel.add(switchPersonPanel);
-
+		mainPanel.add(userDataPanel);
+		
 		mainButtonPanel.add(mainButtonLabel);
-
 		subButtonPanel.add(subButtonLabel);
-
 		mainButtonPanel.add(mainStartStopButton);
-
 		// mainButtonPanel.add(mainStopButton);
-
 		subButtonPanel.add(subStartStopButton);
+		
 
 		// subButtonPanel.add(subStopButton);
-
 		switchPersonPanel.add(upButton);
-
 		switchPersonPanel.add(downButton);
 
 		// Property
-
 		mainButtonLabel.setHorizontalAlignment(SwingConstants.CENTER); // label
 
 		// //
-
 		subButtonLabel.setHorizontalAlignment(SwingConstants.CENTER);
 
 		// event
 
 		reverseTimer mainActionListener = new MyTimer().new reverseTimer();
-
 		sequenceTimer subActionLisener = new MyTimer().new sequenceTimer();
 
 		mainStartStopButton.addActionListener(mainActionListener);
-
 		subStartStopButton.addActionListener(subActionLisener);
 
 		// this.setLayout(new BoxLayout(this.getContentPane(),
-
 		// BoxLayout.X_AXIS));
-
 		// mainPanel.setLayout(new BoxLayout(this.getContentPane(),
-
 		// BoxLayout.X_AXIS));
 
 		mainPanel.setVisible(true);
-
 		this.add(mainPanel);
-
 		this.setVisible(true);
 
 	}
