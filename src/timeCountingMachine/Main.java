96a8ed2ea4785553286f0049e65aee96bb0ad216
gml61 <gml61@DESKTOP-JNS3VFA>
2017-08-23 21:51:11 +0900

    경로이상

--- src/timeCountingMachine/Main.java
+++ src/timeCountingMachine/Main.java
@@ -4,13 +4,15 @@ import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
+
 	public static ShowFrame showFrame;
 	public static ControlFrame controlFrame;
 	public static void main(String[] arguis) {
+
+		System.out.println("1¹ø");
 		// File test and load data.
 		FileManager.loadUserData();
 		FileManager.saveData();
-		
 		// Show frames.
 		showFrame = new ShowFrame();
 		controlFrame = new ControlFrame();
